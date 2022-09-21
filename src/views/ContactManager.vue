75b9a5d14aacc1e0c64e634374a812d4690ebcf5
KokoFibo <kokonacci@gmail.com>
2022-09-21 14:46:48 +0700

    done install axios, bootstrap, fontawesome dan ubuntu google fonts

---
+++ src/views/ContactManager.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>contact manager</h1>
+  <button class="btn btn-primary">click</button>
+  <i class="fa fa-home"></i>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
